4bd40bdf9eaab350292ef87fc4538ee74fd9ecb4
MingjieLuAMD <128463336+mingjielu@users.noreply.github.com>
2025-09-30 15:41:37 +0800

    Update reproduce_mm.py

--- reproduce_mm.py
+++ reproduce_mm.py
@@ -3,7 +3,9 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 
 configs = [
-        {'tensor_size_a': (1668,3584),  'dtype_a': torch.bfloat16, 'tensor_size_b': (3584,18944), 'dtype_b': torch.bfloat16},
+        {'tensor_size_a': (1668,3584),  'dtype_a': torch.bfloat16, 'tensor_size_b': (3584, 18944), 'dtype_b': torch.bfloat16},
+        {'tensor_size_a': (1668,18944),  'dtype_a': torch.bfloat16, 'tensor_size_b': (18944,3584), 'dtype_b': torch.bfloat16},
+        #{'tensor_size_a': (512,1668),  'dtype_a': torch.bfloat16, 'tensor_size_b': (1668,3584), 'dtype_b': torch.bfloat16},
         ]
 
 for config in configs:
