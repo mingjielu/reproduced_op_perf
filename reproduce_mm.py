ab440802adead83971c7276e027ba2444576069c
MingjieLuAMD <128463336+mingjielu@users.noreply.github.com>
2025-09-30 16:00:27 +0800

    Update reproduce_mm.py

--- reproduce_mm.py
+++ reproduce_mm.py
@@ -5,7 +5,7 @@ from torch.profiler import profile, record_function, ProfilerActivity
 configs = [
         {'tensor_size_a': (1668,3584),  'dtype_a': torch.bfloat16, 'tensor_size_b': (3584, 18944), 'dtype_b': torch.bfloat16},
         {'tensor_size_a': (1668,18944),  'dtype_a': torch.bfloat16, 'tensor_size_b': (18944,3584), 'dtype_b': torch.bfloat16},
-        #{'tensor_size_a': (512,1668),  'dtype_a': torch.bfloat16, 'tensor_size_b': (1668,3584), 'dtype_b': torch.bfloat16},
+        {'tensor_size_a': (1668,3584),  'dtype_a': torch.bfloat16, 'tensor_size_b': (3584, 3584), 'dtype_b': torch.bfloat16},
         ]
 
 for config in configs:
@@ -18,11 +18,11 @@ for config in configs:
 
     for _ in range(warmup):
         c = torch.mm(a, b)
-        #c.sum().backward()
+        c.sum().backward()
 
 
     with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], with_stack=True) as prof:
         for _ in range(repeats):
             c = torch.mm(a , b)  # ~H~V使~T torch.add(a, b)
-            #c.sum().backward()
+            c.sum().backward()
     print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
